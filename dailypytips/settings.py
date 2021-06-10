e978ce203bce0090eaf20621b1871dbb269cad29
saviganga <akatugbasaviour@gmail.com>
2021-06-10 06:46:39 -0700

    config.yml config for aws rds dadabase migration

--- dailypytips/settings.py
+++ dailypytips/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': '5432,'
+        'PORT': '5432',
     }
 }
 
