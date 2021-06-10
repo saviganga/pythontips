e49e9ba1e404413bede18e7d021a5b442b12da9f
saviganga <akatugbasaviour@gmail.com>
2021-06-10 11:28:24 -0700

    edit models tip lenght

--- tips/models.py
+++ tips/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Tweet(models.Model):
 
-    username = models.CharField(max_length=140, blank=False, null=False)
-    tip = models.CharField(max_length=140, blank=False, null=False)
+    username = models.CharField(max_length=100, blank=False, null=False)
+    tip = models.CharField(max_length=200, blank=False, null=False)
     email = models.EmailField(max_length=200, blank=True, null=True)
     created_at = models.DateTimeField(max_length=100, auto_now_add=True,)
     updated = models.DateTimeField(auto_now=True, blank=True)
