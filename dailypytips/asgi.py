4033aca384014984f72a82f24f893dd541d2a68e
saviganga <akatugbasaviour@gmail.com>
2021-06-22 05:53:04 -0700

    asgi

--- dailypytips/asgi.py
+++ dailypytips/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dailypytips.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dailypytips.settings.prod')
 
 application = get_asgi_application()
